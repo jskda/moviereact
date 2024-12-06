4544e49b73b5478b8cff59f524d1182cd85b3288
jskda <railschandevelop@gmail.com>
2024-12-07 03:15:48 +0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -9,17 +9,17 @@ function App() {
 
   // Используем useEffect для получения данных с API
   useEffect(() => {
-    console.log('API URL:', API_URL);
-    fetch(`${API_URL}/api/movies`)
+    // console.log('API URL:', API_URL);
+    fetch(`${API_URL}/api/movies?api_token=YOUR_API_TOKEN&direction=desc&limit=10&page=1`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);  // Посмотреть в консоли, что приходит от API
-        setMovies(data.movies);  // Обновить состояние
+        setMovies(data.data);  // Обновить состояние
       })
       .catch((error) => {
         console.error('Error fetching movies:', error);
       });
-  }, []);  // Пустой массив зависимостей, чтобы хук сработал только при монтировании компонента
+  });  // Пустой массив зависимостей, чтобы хук сработал только при монтировании компонента
 
   return (
     <Router>
