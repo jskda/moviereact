a164da5b5e62b7169aeeffb751719cbccb7399c2
jskda <railschandevelop@gmail.com>
2024-12-07 00:11:43 +0400

    Add CORS

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 require("dotenv").config(); // Для работы с переменными окружения
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 const morgan = require("morgan");
 app.use(morgan("dev"));
 
